38c34e64b32b4540273720caf0c070a3fe2ba861
septaw466-ops <septaw466@gmail.com>
2026-08-28 02:18:28 +0700

    ubah

--- lib/main.dart
+++ lib/main.dart
@@ -114,7 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Klik Bang',
+        tooltip: 'Klik cuy',
         child: const Icon(Icons.add),
       ),
     );
